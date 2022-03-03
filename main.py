305f957a995eca2e9f1cfb41a2961a0d3b55e20a
danil <danil.vladimirow2016@yandex.ru>
2022-03-03 16:44:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from data import db_session
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+db_session.global_init(input())
+db_session = db_session.create_session()
+
+for user in db_session.query(User).all():
+    if user.address == 'module_1':
+        print(user)
+
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+global_init(input())
+
+for job in create_session().query(Jobs).all():
+    if job.work_size < 20 and job.is_finished != True:
+        print(f'<Job> {job.job}')
